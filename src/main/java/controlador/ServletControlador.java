6ec632f9a6077a93f311e0ceeb228795ae508539
Francisco Jimenez <thefran0803@gmail.com>
2020-03-31 12:15:57 -0600

    Initial commit

---
+++ src/main/java/controlador/ServletControlador.java
@@ -0,0 +1,27 @@
+package controlador;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //1. Procesamos los parámetros
+        
+        //2. Creamos los JavaBeans
+        
+        //3. Agregamos el Java beans a algun alcance
+        request.setAttribute("mensaje", "saludos desde el Servlet");
+        
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("mensaje2", "¡Hola Mundo!");
+        
+        //4. redireccionar a la vista seleccionada
+        RequestDispatcher rd = request.getRequestDispatcher("vista/desplegarPantallaPrincipal.jsp");
+        rd.forward(request, response);
+    }
+    
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>442 Online Shop</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>442 Online Shop</h1>
+        <br/>
+        <a href="${pageContext.request.contextPath}/ServletControlador">
+            Link al servlet controlador que despliega la vista #1
+        </a>
+    </body>
+</html>
+
